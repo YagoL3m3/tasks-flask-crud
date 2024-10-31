900139c7b3f547760ca25cd747dfbe30e51d3c76
Yago da Silva Leme <143760057+YagoL3m3@users.noreply.github.com>
2024-10-31 17:10:03 -0300

    Criação dos outros Metodos HTTP
    Criação dos outros Metodos HTTP
    PUT, DELETE, GET

--- app.py
+++ app.py
@@ -16,5 +16,56 @@ def creat_task():
     print(tasks)
     return jsonify ({"message": "Nova tarefa criada com sucesso"})
 
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    task_list = [task.to_dict() for task in tasks]
+
+    output = {
+                "tasks": task_list,
+                "total_tasks": len (task_list)
+    }
+    return jsonify(output)
+
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+    for t in tasks:
+        if t.id ==id:
+            return jsonify(t.to_dict())
+    return jsonify({"message": "Não foi possivel encontrar a atividade"}), 404
+
+@app.route('/tasks/<int:id>', methods= ["PUT"])
+def update_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+    print(task)
+
+    if task == None:
+        return jsonify({"message": "Não foi possivel encontrar a atividade"}), 404
+    
+    data = request.get_json()
+    task.title = data['title']
+    task.description = data['description']
+    task.completed = data['completed']
+    print(task)
+    return jsonify ({"message": "Tarefa atualizada com sucesso"}), 200
+
+@app.route('/tasks/<int:id>', methods= ["DELETE"])
+def delete_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+    
+    if not task:
+        return jsonify({"message": "Não foi possivel encontrar a atividade"}), 404
+    
+    tasks.remove(task)
+    return jsonify ({"message": "Tarefa deletada com sucesso"})
+
+
 if __name__ == "__main__":
     app.run(debug=True)
