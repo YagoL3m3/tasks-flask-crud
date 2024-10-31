fecad8c38f8be36647bd8185c643d4c91c4368b2
Yago da Silva Leme <143760057+YagoL3m3@users.noreply.github.com>
2024-10-31 10:03:43 -0300

    Criação da Função POST
    Criação da função POST e também da pasta Models com as classes que estão sendo importadas

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.task import task
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello world!!"
+tasks = []
+task_id_control = 1
 
-@app.route("/about")
-def about():
-    return "Pagina SObre"
-
-app.run(debug=True)
+@app.route('/tasks', methods=['POST'])
+def creat_task():
+    global task_id_control
+    data = request.get_json()
+    new_task = task(id=task_id_control, title = data['title'], description=data.get("description", ""))
+    task_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify ({"message": "Nova tarefa criada com sucesso"})
 
 if __name__ == "__main__":
     app.run(debug=True)
